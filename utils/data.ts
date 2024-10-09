7313ff7898c1cce8e0be651dcdc52030b3750fe6
Stephane Mfuni <stephuni35@gmail.com>
2024-10-09 06:03:44 +0300

    added stuns google server

--- utils/data.ts
+++ utils/data.ts
@@ -2,8 +2,13 @@ export const SERVER_URL = process.env.SERVER_URL;
 
 export const iceServers = [
   { urls: "stun:stun.l.google.com:19302" },
-  { urls: "stun:stun1.l.google.com:19302" },
+  { urls: "stun:stun.l.google.com:5349" },
+  { urls: "stun:stun1.l.google.com:3478" },
+  { urls: "stun:stun1.l.google.com:5349" },
   { urls: "stun:stun2.l.google.com:19302" },
-  { urls: "stun:stun3.l.google.com:19302" },
+  { urls: "stun:stun2.l.google.com:5349" },
+  { urls: "stun:stun3.l.google.com:3478" },
+  { urls: "stun:stun3.l.google.com:5349" },
   { urls: "stun:stun4.l.google.com:19302" },
+  { urls: "stun:stun4.l.google.com:5349" },
 ];
